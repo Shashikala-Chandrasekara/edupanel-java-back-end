f9e6464e9d88ee9d926eba5bd3fe347d8970d0d3
shashikala <chandrasekaremsmh@gmail.com>
2023-12-04 17:16:28 +0530

    Create custom validation constraint

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,11 +1,27 @@
 package lk.ijse.dep11.edupanel;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan
 @Configuration
 @EnableWebMvc
 public class WebAppConfig {
+
+
+    // old one
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver(){
+//        return new CommonsMultipartResolver();
+//    }
+
+    // new one
+    @Bean
+    public StandardServletMultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
 }
--- src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -1,14 +1,20 @@
 package lk.ijse.dep11.edupanel.api;
 
+import lk.ijse.dep11.edupanel.to.request.LecturerRequestTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/lecturers")
 @CrossOrigin
 public class LecturerHttpController {
 
-    @PostMapping
-    public void createNewLecturer(){
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "multipart/form-data", produces = "application/json")
+    public void createNewLecturer(@ModelAttribute @Valid LecturerRequestTO lecturer){
+        System.out.println(lecturer);
         System.out.println("createLecturer()");
     }
 
